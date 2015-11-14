eedfc64f60d499712d499c8619e1ed5a84971d44
Rohith24 <rohithreddy24@hotmail.com>
2015-11-14 16:30:37 +0530

    Submit Commit

--- src/KthIndexFromEnd.cpp
+++ src/KthIndexFromEnd.cpp
@@ -10,7 +10,25 @@ ERROR CASES: Return '\0' for invalid inputs.
 
 NOTES:
 */
+int strlength(char *str);
 
 char KthIndexFromEnd(char *str, int K) {
-	return '\0';
+	if (str==0||str==""||K<0)
+		return '\0';
+	else
+	{
+		int strlen = strlength(str);
+		if (strlen < K){
+			return '\0';
+		}
+		return str[strlen - K - 1];			//get element from last
+	}
+}
+
+int strlength(char *str){
+	int counter = 0;
+	while (str[counter] != '\0')
+		counter++;
+	return counter;
+
 }
--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -12,6 +12,46 @@ NOTES: Don't create new string.
 #include <Stdio.h>
 #include <string.h>
 
+void reverseWord(char *inp, int st, int end);
+void swap(char *input, int i, int j);
 void str_words_in_rev(char *input, int len){
+	int counter1, counter2, count = 0;
+	for (counter1 = 0, counter2 = len - 1; counter1 < counter2 && counter1 != counter2; counter1++, counter2--)
+		swap(input, counter1, counter2);
+
+	for (counter1 = 0, counter2 = 0; counter1 <= counter2&&input[counter2] != '\0';) {
+		if (input[counter2] != ' '){
+			counter2++;
+		}
+		else{
+			count++;
+			reverseWord(input, counter1, counter2 - 1);
+			counter2++;
+
+			counter1 = counter2;
+		}
+	}
+
+
+
+	if (count == 0){
+		reverseWord(input, 0, len - 1);
+		return;
+	}
+
+	reverseWord(input, counter1, counter2 - 1);
 	
+}
+
+void swap(char *input, int i, int j){
+	char temp = input[i];
+	input[i] = input[j];
+	input[j] = temp;
+}
+
+void reverseWord(char *input, int counter1, int counter2){
+
+	char temp;
+	for (; counter1 < counter2 && counter1 != counter2; counter1++, counter2--)
+		swap(input, counter1, counter2);
 }
