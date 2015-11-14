77acc00e6229761f9aff45df033e23b32b15afc6
Rohith24 <rohithreddy24@hotmail.com>
2015-11-14 18:18:45 +0530

    Submit Commit

--- src/removeSpaces.cpp
+++ src/removeSpaces.cpp
@@ -11,6 +11,48 @@ ERROR CASES: Return '\0' for invalid inputs.
 NOTES: Don't create new string.
 */
 
+int strlen(char* str);
+void removeSpace(char *str,int p);
+int findSpace(char *str,int st);
+
+
 char removeSpaces(char *str) {
-	return '\0';
+	if (str == ""||str==0)
+		return '\0';
+	else
+	{
+		int st = 0;
+		while (str[st] != ' '&&str[st] != '\0'){
+			st++;
+		}
+		if (st == strlen(str))
+			return ' ';
+		removeSpace(str, st);
+		while (st < strlen(str)){
+			st=findSpace(str, st);
+			removeSpace(str, st);
+		}
+	}
+}
+
+void removeSpace(char *str,int p){
+	int i,len=strlen(str);
+	for (i = p; i<len - 1; i++)
+		str[i] = str[i + 1];
+	len -= 1;
+	str[len] = '\0';
+}
+
+int strlen(char* str){
+	int counter = 0;
+	while (str[counter] != '\0')
+		counter++;
+	return counter;
+}
+
+int findSpace(char *str,int st){
+	while (str[st] != ' '){
+		st++;
+	}
+	return st;
 }
